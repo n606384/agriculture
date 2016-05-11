6b7c3dbbacd7136850319581ae8debb1a81d846d
n606384 <n606384@163.com>
2016-05-11 14:54:18 +0800

    hjgl-2

--- AgricultureR/assets/js/hjgl-2.js
+++ AgricultureR/assets/js/hjgl-2.js
@@ -12,13 +12,13 @@
 		initPage();
 		
 	}
-	function centerModals() {
-	    $('#updateModal').each(function(i) { 
+	function centerModals(){
+	    $('#updateModal').each(function(i) {
 	        var $clone = $(this).clone().css('display', 'block').appendTo('body'); var top = Math.round(($clone.height() - $clone.find('.modal-content').height()) / 2);   
 	        top = top > 0 ? top : 0;   
 	        $clone.remove();   
 	        $(this).find('.modal-content').css("margin-top", top);
-	    });   
+	    });
 	}
 	function initPage(){
 		
@@ -27,7 +27,7 @@
 		
 		$('#updateModal').on('show.bs.modal', centerModals);
 					
-		$("#updateModal").draggable({   
+		$("#updateModal").draggable({
 		    handle: ".modal-header",   
 		    cursor: 'move',   
 		    refreshPositions: false  
