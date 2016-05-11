0f92a638c30884cce2bb91a58844e058f0b8e017
n606384 <n606384@163.com>
2016-05-11 16:00:53 +0800

    login修改

--- AgricultureR/js/login.js
+++ AgricultureR/js/login.js
@@ -19,17 +19,7 @@ $(function(){
 					console.log(app.token);
 					flag = true;
 					window.location.href = "hjgl.html?token="+app.token;
-					$.ajax({
-						type:"post",
-						url:"http://192.168.44.232:8080/rest/hjyy/getHJDWRYXX?token="+app.token,
-						async:true,
-						data:{
-							"ssqx":110105
-						},
-						success:function(data){
-							console.log("用户列表",data);
-						}
-					});
+					
 				}else if(res.login === "error"){
 					document.getElementById("tips").innerText = "用户名或密码错误，请重新输入！";
 					$('#tips').text("用户名或密码错误，请重新输入！");
